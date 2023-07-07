4274a695b73652be64033d0af8ef560c43a268e7
Piyush Dahle <57090813+PIYUSHDAHLE@users.noreply.github.com>
2023-07-07 22:27:49 +0530

    Create Sum of First N Natural numbers.cpp

---
+++ Sum of First N Natural numbers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number : "; 
+    cin >> n;
+    
+    int sum=0;
+    
+    for(int i=1;i<=n;i++) 
+        sum+=i;
+
+    cout << sum;
+
+    return 0;
+}
